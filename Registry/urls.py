7bf1363d37412cee0e8825602406027e9bbe36dd
kleicezar <140003646+kleicezar@users.noreply.github.com>
2024-12-26 16:57:37 -0500

    update sale and clients

--- Registry/urls.py
+++ Registry/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
     path('prsn/upt/<int:id_client>/', update_client, name='update_client'),  # Atualizar cliente
     path('prsn/del/<int:id_client>/', delete_client, name='delete_client'),
 
+    # path('tcnch/', Technician_list, name='Technician'),
+    # path('c/tcnch/', Technician_Create, name='Technician_Create'),
+    # path('b-tcnch/', buscar_Ttchnicianes, name='buscar_Ttchnicianes'),
+    # path('tcnch/upt/<int:idtTechnician>/', updatetTechnician, name='updatetTechnician'),  # Atualizar technician
+    # path('tcnch/del/<int:idtTechnician>/', deletetTechnician, name='deletetTechnician'),
+
     # path('Spl/', supplier, name='Supplier'),
     # path('SplForm/', supplierForm, name='SupplierForm'),
     # path('Spl/upt/<int:id_supplier>/', updateSupplier, name='updateSupplier'),
