979c9cecfa73f56c326e2b626cf7eb236e92ebb1
Gabriel Logan <142037370+LoganGabi@users.noreply.github.com>
2025-02-02 20:01:47 -0500

    Ajeitando bug menu

--- login/static/js/menu.js
+++ login/static/js/menu.js
@@ -9,31 +9,38 @@ toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
     content.classList.toggle('expanded'); // Alterna a classe no conteúdo
 });
-let isToggle = true;
+
 // Adiciona eventos de clique para os links do menu
 toggles.forEach(toggle => {
     toggle.addEventListener('click', function(event) {
         event.preventDefault(); // Impede o comportamento padrão do link
 
         const subMenu = this.nextElementSibling; // Seleciona a próxima sub-menu
-
+        // let isToggle = true;
         // Alterna a exibição da sub-menu
         if (subMenu.style.display === 'block') {
-            subMenu.style.display = 'none';
+            
             // toggle.classList.toggle('toggle');
+            subMenu.style.display = 'none';
+
+            toggle.classList.replace("rotate", "toggle");
+           
+            // console.log(meuElemento.className); // "classe3 classe2"
+
         } else {
+            toggle.classList.replace( "toggle","rotate");
             subMenu.style.display = 'block';
-            // toggle.classList.toggle('rotate');
+          
         }
         /*nao colocar antes do if e do else de cima*/
-        if(isToggle){
-            toggle.classList.toggle("rotate");
-            isToggle = !isToggle;
-        }
-        else{
-            toggle.classList.toggle("toggle");
-            isToggle = !isToggle;
-        }
+        // if(isToggle){
+        //     toggle.classList.toggle("rotate");
+        //     isToggle = !isToggle;
+        // }
+        // else{
+        //     toggle.classList.toggle("toggle");
+        //     isToggle = !isToggle;
+        // }
     });
 });
 
