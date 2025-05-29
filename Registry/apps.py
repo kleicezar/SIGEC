50a1485cc7f0dd3b6be9a2b2227fe14720af88cb
Gabriel Logan <142037370+LoganGabi@users.noreply.github.com>
2025-05-28 21:13:33 -0500

    Renomeação de pastas - sale,registry,service

--- Registry/apps.py
+++ Registry/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class registryConfig(AppConfig):
+class RegistryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registry'
--- Sale/apps.py
+++ Sale/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class saleConfig(AppConfig):
+class SaleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sale'
 
