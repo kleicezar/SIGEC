cbd45a39642f37281693488f895299a79cf9a856
Gabriel Logan <142037370+LoganGabi@users.noreply.github.com>
2025-06-03 18:33:40 -0500

    merge success

--- Registry/models.py
+++ Registry/models.py
@@ -48,15 +48,11 @@ class Person(models.Model):
     PersonalPhone = models.CharField('Telefone Pessoal', max_length=100)
     isActive = models.BooleanField('isActive', max_length=100)
     site = models.CharField('site', max_length=100,null=True, blank=True) 
-<<<<<<< HEAD
     salesman = models.CharField('Vendedor', max_length=100,null=True, blank=True)
     creditLimit = models.DecimalField('Limite de Crédito', max_length=100, decimal_places=2, max_digits=10,default=0)
-=======
-    salesman = models.CharField('salesman', max_length=100,null=True, blank=True)
-    creditLimit = models.DecimalField('creditLimit', max_length=100, decimal_places=2, max_digits=10,default=0)
     email = models.EmailField('E-mail', max_length=128, null=True, blank=True) 
     password = models.CharField('Senha', max_length=128, null=True, blank=True) 
->>>>>>> 4dc425c75abafbb664c2b6bb29ce408cea06b154
+
     isClient = models.BooleanField("Cliente",null=True, blank=True)
     isSupllier = models.BooleanField("Fornecedor",null=True, blank=True)
     isUser = models.BooleanField("Usuario do Sistema",null=True, blank=True)
