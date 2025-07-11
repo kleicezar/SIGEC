279957d19bd79e919a8be085d93b29944cf5b6c3
kleicezar <140003646+kleicezar@users.noreply.github.com>
2025-07-11 00:12:38 -0500

    Delete Registry directory

--- Registry/static/js/mask.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.mask-cpf').mask('000.000.000-00', {reverse: true});
-    $('.mask-date').mask("00/00/0000", {placeholder: "__/__/____"})
-    $('.mask-cep').mask("00000-000")
-    
-})
